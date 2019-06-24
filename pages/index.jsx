87f6e689e0afd5246383cac6c6927dc1c1d6be17
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-06-24 19:49:28 -0300

    setup project

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default () => (
+  <div>
+    <Title>My First Next.js Page</Title>
+  </div>
+);
