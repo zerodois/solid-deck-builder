3df673d8738c7676c20a912534f82902b913840a
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2019-07-02 14:38:34 -0300

    change to List

--- services/store/index.js
+++ services/store/index.js
@@ -12,19 +12,19 @@ export const init = async (store, webId) => {
   const fetcher = new $rdf.Fetcher(store, {});
   const typeIndex = store.any(profile, SOLID('publicTypeIndex'), undefined, undefined);
   await fetcher.load(typeIndex);
-  const registry = store.any(undefined, SOLID('forClass'), BOOKMARK('Bookmark'), undefined);
-  console.log('REG', BOOKMARK('Bookmark'));
-  return;
-  if (registry) {
-    return registry;
-  }
+  // const registry = store.any(undefined, SOLID('forClass'), BOOKMARK('Bookmark'), undefined);
+  // console.log('PORRA', registry);
+  // const registry = store.any(undefined, SOLID('forClass'), BOOKMARK('Bookmark'), undefined);
+  // if (registry) {
+  //   return registry;
+  // }
   const storage = store.any(profile, PIM('storage'), undefined, undefined);
-  const name = `${storage.value}public/deckbuilder.ttl`;
-  const collIndex = $rdf.sym(`${typeIndex.value}#deckbuilder`);
+  const name = `${storage.value}public/mtg-collection.ttl`;
+  const collIndex = $rdf.sym(`${typeIndex.value}#mtg-collection`);
   const insertions = [
     $rdf.st(typeIndex, DC('references'), collIndex, typeIndex),
     $rdf.st(collIndex, RDF('type'), SOLID('TypeRegistration'), collIndex.doc()),
-    $rdf.st(collIndex, SOLID('forClass'), BOOKMARK('Bookmark'), collIndex.doc()),
+    $rdf.st(collIndex, SOLID('forClass'), RDF('List'), collIndex.doc()),
     $rdf.st(collIndex, SOLID('instance'), $rdf.sym(name), collIndex.doc()),
   ];
   const updater = new $rdf.UpdateManager(store);
